d14a2405f969b8731157fc2efdef08c45d4a4bd5
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2019-12-23 15:29:16 +0000

    Remove require line in dummy app
    In WEX and WCR when we mount an engine we are not required to add this require statement. And as its our intent to have the engine mount and work in the same way as those projects, this change alters the dummy to be consistent with them.

--- spec/dummy/config/application.rb
+++ spec/dummy/config/application.rb
@@ -9,7 +9,6 @@ require "sprockets/railtie"
 # require "rails/test_unit/railtie"
 
 Bundler.require(*Rails.groups)
-require "defra_ruby_mocks"
 
 module Dummy
   class Application < Rails::Application

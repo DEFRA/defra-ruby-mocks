c6002ef07f9762e0ec7176358a9176a5353c4c0a
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2019-12-24 11:04:31 +0000

    Add configuration to the project
    When we implemented mocks before, we have also provided a way to both enable the mocks (or disable them) but also implement a delay in the response.
    
    By default we don't want the mocks enabled to protect against them accidently being used in production. And because we are not crunching through some massive database or carrying out any complex logic, our mock responses are likely to be far quicker than the real ones. So we also provide a means of introducing a configurable delay in the mock response.

--- lib/defra_ruby_mocks.rb
+++ lib/defra_ruby_mocks.rb
@@ -3,4 +3,38 @@
 require "defra_ruby_mocks/engine"
 
 module DefraRubyMocks
+  # Enable the ability to configure the gem from its host app, rather than
+  # reading directly from env vars. Derived from
+  # https://robots.thoughtbot.com/mygem-configure-block
+  class << self
+    attr_writer :configuration
+
+    def configuration
+      @configuration ||= Configuration.new
+    end
+
+    # Added for testing. Without we cannot test both a config object with and
+    # with set values in the same rspec session
+    def reset_configuration
+      @configuration = nil
+    end
+  end
+
+  def self.configure
+    yield(configuration)
+  end
+
+  class Configuration
+    # Set whether the mocks are enabled. Only if set to true will the mock
+    # pages be accessible
+    attr_accessor :enabled
+    # Set a delay in milliseconds for the mocks to respond.
+    # Defaults to 1000 (1 sec)
+    attr_accessor :delay
+
+    def initialize
+      @enabled = false
+      @delay = 1000
+    end
+  end
 end

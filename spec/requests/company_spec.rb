a4139f4ea68bd814284f8e88e3d33b225d84bba1
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2019-12-24 12:53:32 +0000

    Add test for disabled mocks
    Ensure we have a test that confirms if mocks are disabled you cannot access them.

--- spec/requests/company_spec.rb
+++ spec/requests/company_spec.rb
@@ -4,55 +4,17 @@ require "rails_helper"
 
 module DefraRuby
   RSpec.describe "Company", type: :request do
-    before(:all) { Helpers::Configuration.prep_for_tests }
-    after(:all) { Helpers::Configuration.reset_for_tests }
 
     let(:path) { "/defra_ruby_mocks/company" }
 
-    context "when the company number is 99999999 for not found" do
-      let(:company_number) { "99999999" }
+    context "when mocks are enabled" do
+      before(:all) { Helpers::Configuration.prep_for_tests }
+      after(:all) { Helpers::Configuration.reset_for_tests }
 
-      it "returns a JSON response with a code of 404" do
-        get "#{path}/#{company_number}"
-        content = JSON.parse(response.body)
+      context "when the company number is 99999999 for not found" do
+        let(:company_number) { "99999999" }
 
-        expect(response.content_type).to eq("application/json")
-        expect(response.code).to eq("404")
-        expect(content).to include("errors")
-      end
-    end
-
-    context "when the company number is from the 'specials' list" do
-      let(:company_number) { "05868270" }
-
-      it "returns a JSON response with a 200 code and a status that isn't 'active'" do
-        get "#{path}/#{company_number}"
-        company_status = JSON.parse(response.body)["company_status"]
-
-        expect(response.content_type).to eq("application/json")
-        expect(response.code).to eq("200")
-        expect(company_status).not_to eq("active")
-      end
-    end
-
-    context "when the company number is not from the 'specials' list" do
-      context "and it is valid" do
-        let(:company_number) { "SC247974" }
-
-        it "returns a JSON response with a 200 code and a status of 'active'" do
-          get "#{path}/#{company_number}"
-          company_status = JSON.parse(response.body)["company_status"]
-
-          expect(response.content_type).to eq("application/json")
-          expect(response.code).to eq("200")
-          expect(company_status).to eq("active")
-        end
-      end
-
-      context "and it is not valid" do
-        let(:company_number) { "foo" }
-
-        it "returns a JSON response with a 404 code" do
+        it "returns a JSON response with a code of 404" do
           get "#{path}/#{company_number}"
           content = JSON.parse(response.body)
 
@@ -61,6 +23,58 @@ module DefraRuby
           expect(content).to include("errors")
         end
       end
+
+      context "when the company number is from the 'specials' list" do
+        let(:company_number) { "05868270" }
+
+        it "returns a JSON response with a 200 code and a status that isn't 'active'" do
+          get "#{path}/#{company_number}"
+          company_status = JSON.parse(response.body)["company_status"]
+
+          expect(response.content_type).to eq("application/json")
+          expect(response.code).to eq("200")
+          expect(company_status).not_to eq("active")
+        end
+      end
+
+      context "when the company number is not from the 'specials' list" do
+        context "and it is valid" do
+          let(:company_number) { "SC247974" }
+
+          it "returns a JSON response with a 200 code and a status of 'active'" do
+            get "#{path}/#{company_number}"
+            company_status = JSON.parse(response.body)["company_status"]
+
+            expect(response.content_type).to eq("application/json")
+            expect(response.code).to eq("200")
+            expect(company_status).to eq("active")
+          end
+        end
+
+        context "and it is not valid" do
+          let(:company_number) { "foo" }
+
+          it "returns a JSON response with a 404 code" do
+            get "#{path}/#{company_number}"
+            content = JSON.parse(response.body)
+
+            expect(response.content_type).to eq("application/json")
+            expect(response.code).to eq("404")
+            expect(content).to include("errors")
+          end
+        end
+      end
     end
+
+    context "when mocks are disabled" do
+      before(:all) { DefraRubyMocks.configuration.enabled = false }
+
+      let(:company_number) { "SC247974" }
+
+      it "cannot load the page" do
+        expect { get "#{path}/#{company_number}" }.to raise_error(ActionController::RoutingError)
+      end
+    end
+
   end
 end

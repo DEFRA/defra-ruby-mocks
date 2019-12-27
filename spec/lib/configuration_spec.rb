1cc464b58d009732872e5a3326c20084bd4808b4
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2019-12-27 17:40:37 +0000

    Fix Configuration.delay to handle strings (#10)
    A bit like changes we made to `enable` to handle values being passed in as strings rather than integers, we should have spotted that the same will apply to `delay`.
    
    Again we can expect the same usage patterns for setting the config
    
    ```ruby
    DefraRubyMocks::configure do |config|
      config.enable = ENV["ENABLE_MOCKS"] || false
      config.enable = ENV["MOCK_DELAY"] || 1000
    end
    ```
    
    So there is a good chance the value of the env var will just be chucked straight across rather than the host app converting it first to an integer.

--- spec/lib/configuration_spec.rb
+++ spec/lib/configuration_spec.rb
@@ -37,5 +37,39 @@ module DefraRubyMocks
         end
       end
     end
+
+    describe "#delay=" do
+      context "when passed 200 as an integer" do
+        it "sets delay to 200" do
+          subject.delay = 200
+
+          expect(subject.delay).to be(200)
+        end
+      end
+
+      context "when passed 200 as a string" do
+        it "sets delay to 200" do
+          subject.delay = "200"
+
+          expect(subject.delay).to be(200)
+        end
+      end
+
+      context "when passed a string that's not a number" do
+        it "sets delay to its default" do
+          subject.delay = ""
+
+          expect(subject.delay).to be(Configuration::DEFAULT_DELAY)
+        end
+      end
+
+      context "when passed nil" do
+        it "sets delay to its default" do
+          subject.delay = nil
+
+          expect(subject.delay).to be(Configuration::DEFAULT_DELAY)
+        end
+      end
+    end
   end
 end

99cd3d1274372c3c3250d266b0909af0e708604e
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2020-01-15 18:32:02 +0000

    Refactor tests in worldpay response spec
    When about to start adding more tests to this spec we realised we'd need a bunch of mocks setup in order for things to work.
    
    With them in place, we might as well use them for the existing tests, hence this first step is to refactor what we already have before adding the new unit tests.

--- spec/services/worldpay_response_service_spec.rb
+++ spec/services/worldpay_response_service_spec.rb
@@ -5,19 +5,27 @@ require "rails_helper"
 module DefraRubyMocks
   RSpec.describe WorldpayResponseService do
     describe ".run" do
+      let(:relation) { double(:relation) }
+      let(:registration) { double(:registration) }
+      let(:finance_details) { double(:finance_details) }
+      let(:orders) { double(:orders) }
 
       context "when the request comes from the waste-carriers-front-office" do
+        before do
+          allow(::WasteCarriersEngine::TransientRegistration).to receive(:where) { relation }
+          allow(relation).to receive(:first) { registration }
+          allow(registration).to receive(:finance_details) { finance_details }
+          allow(finance_details).to receive(:orders) { orders }
+          allow(orders).to receive(:order_by) { orders }
+          allow(orders).to receive(:first) { order }
+        end
+
         let(:reference) { "12345" }
         let(:success_url) { "http://example.com/fo/#{reference}/worldpay/success" }
+        let(:order) { double(:order, order_code: reference, total_amount: 105_00) }
 
         it "can extract the reference from the `success_url`" do
-          allow(::WasteCarriersEngine::TransientRegistration).to receive(:where)
-
-          # Because we have not setup all the required mocks for this test
-          # we expect this exception to happen. But really we're just
-          # swallowing the error here to allow us to test the next
-          # assertion
-          expect { described_class.run(success_url) }.to raise_error(NoMethodError)
+          described_class.run(success_url)
 
           expect(::WasteCarriersEngine::TransientRegistration).to have_received(:where).with(token: reference)
         end
@@ -30,19 +38,21 @@ module DefraRubyMocks
           # `locate_transient_registration()` to allow the service to then
           # call `locate_registration()`
           allow_any_instance_of(described_class).to receive(:locate_transient_registration).and_return(nil)
+
+          allow(::WasteCarriersEngine::Registration).to receive(:where) { relation }
+          allow(relation).to receive(:first) { registration }
+          allow(registration).to receive(:finance_details) { finance_details }
+          allow(finance_details).to receive(:orders) { orders }
+          allow(orders).to receive(:order_by) { orders }
+          allow(orders).to receive(:first) { order }
         end
 
         let(:reference) { "98765" }
         let(:success_url) { "http://example.com/your-registration/#{reference}/worldpay/success/54321/NEWREG" }
+        let(:order) { double(:order, order_code: reference, total_amount: 105_00) }
 
         it "can extract the reference from the `success_url`" do
-          allow(::WasteCarriersEngine::Registration).to receive(:where)
-
-          # Because we have not setup all the required mocks for this test
-          # we expect this exception to happen. But really we're just
-          # swallowing the error here to allow us to test the next
-          # assertion
-          expect { described_class.run(success_url) }.to raise_error(NoMethodError)
+          described_class.run(success_url)
 
           expect(::WasteCarriersEngine::Registration).to have_received(:where).with(reg_uuid: reference)
         end

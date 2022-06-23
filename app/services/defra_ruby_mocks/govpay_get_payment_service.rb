615449b71341b2784c87b068af6f9af022b70130
PaulDoyle-DEFRA <97455399+PaulDoyle-DEFRA@users.noreply.github.com>
2022-06-23 13:47:31 +0100

    Add Govpay mocks (#104)
    https://eaflood.atlassian.net/browse/RUBY-1890

---
+++ app/services/defra_ruby_mocks/govpay_get_payment_service.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+require "securerandom"
+
+module DefraRubyMocks
+  class GovpayGetPaymentService < BaseService
+
+    def run(payment_id:, amount: Random.rand(100..1_000), created_at: Time.current)
+      {
+        created_date: created_at,
+        amount: amount,
+        state: {
+          status: "success",
+          finished: true
+        },
+        description: "Your waste carriers registration fee",
+        reference: "12345",
+        language: "en",
+        payment_id: payment_id,
+        refund_summary: {
+          status: "available",
+          amount_available: amount - 100,
+          amount_submitted: 0
+        },
+        total_amount: amount,
+        payment_provider: "worldpay",
+        provider_id: "10987654321"
+      }
+    end
+
+  end
+end

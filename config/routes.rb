7ad66605db8a1ca9ac88c2b1bbcba9d9ef97cc6e
PaulDoyle-DEFRA <97455399+PaulDoyle-DEFRA@users.noreply.github.com>
2023-03-02 16:09:07 +0000

    Remove redundant Worldpay mocks (#110)
    https://eaflood.atlassian.net/browse/RUBY-1980

--- config/routes.rb
+++ config/routes.rb
@@ -11,16 +11,6 @@ DefraRubyMocks::Engine.routes.draw do
       as: "company_officers",
       constraints: ->(_request) { DefraRubyMocks.configuration.enabled? }
 
-  post "/worldpay/payments-service",
-       to: "worldpay#payments_service",
-       as: "worldpay_payments_service",
-       constraints: ->(_request) { DefraRubyMocks.configuration.enabled? }
-
-  get "/worldpay/dispatcher",
-      to: "worldpay#dispatcher",
-      as: "worldpay_dispatcher",
-      constraints: ->(_request) { DefraRubyMocks.configuration.enabled? }
-
   post "/govpay/v1/payments",
        to: "govpay#create_payment",
        as: "govpay_create_payment",

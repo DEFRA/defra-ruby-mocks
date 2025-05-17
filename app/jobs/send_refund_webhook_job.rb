b36c98e6988680ad893f664f0ddc887b88199c14
PaulDoyle-DEFRA <97455399+PaulDoyle-DEFRA@users.noreply.github.com>
2025-05-17 17:00:15 +0100

    Fix assignment of govpay_id in send_refund_webhook_job

--- app/jobs/send_refund_webhook_job.rb
+++ app/jobs/send_refund_webhook_job.rb
@@ -11,7 +11,7 @@ class SendRefundWebhookJob < BaseSendWebhookJob
   def webhook_body(govpay_id:, status:)
     webhook_body ||= JSON.parse(File.read("lib/fixtures/files/govpay/webhook_refund_update_body.json"))
 
-    webhook_body["payment_id"] = govpay_id
+    webhook_body["refund_id"] = govpay_id
     webhook_body["status"] = status
 
     webhook_body
